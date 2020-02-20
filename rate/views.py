5117263cdb42f1d569ace91f851b9ddd7ddf437a
jiss02 <olo5112olo@naver.com>
2020-02-20 18:45:44 +0900

    url 설정

---
+++ rate/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def readAll(req, res):
+    return 404
+
+def create(req, res):
+    return 404
--- teacherTest/urls.py
+++ teacherTest/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/',include('member.urls')),
+    path('rate/', include('rate.urls')),
 ]
