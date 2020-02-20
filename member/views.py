67709233c2ee444dd8d7ded4c0de748130126b65
likelion-guinness <harukawa99@likelion.org>
2020-02-20 16:53:22 +0900

    Implement Signup

--- member/views.py
+++ member/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import User
+from django.contrib import auth
+
 
 def signup(request):
+    if request.method == "POST":
+        if request.POST['password']  == request.POST['password_re']:
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password'],
+                grade = request.POST['grade'])
+            auth.login(request, user)
+        return render(request,'member/login.html')
     return render(request, 'member/signup.html')
 
 def login(request):
