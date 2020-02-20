993d69853ac8db7d87a204deac191d8c57bde3cf
jiss02 <olo5112olo@naver.com>
2020-02-20 19:37:58 +0900

    create 함수 및 read 함수 보완

--- rate/views.py
+++ rate/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Rate
 from .forms import RateForm
 
-def readAll(req, res):
-    rates = Rate.objects.all()
+def readAll(req):
+    rates = Rate.objects.all().order_by("-created_at")
     return render(req, 'rate/list.html', {'rates':rates})
 
-def create(req, res):
+def create(req):
     if req.method == "POST":
         form = RateForm(req.POST)
         if form.is_valid():
-            form.save()
+            rate = form.save(commit=False)
+            rate.user = req.user
+            rate.save()
             return redirect('rate_list')
     else:
         form = RateForm()
