bffff40e94dffe9261e1db4675aef51be2cfa1d3
jiss02 <olo5112olo@naver.com>
2020-02-20 18:39:16 +0900

    rate 모델 생성

---
+++ rate/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from member.models import User
+
+# Create your models here.
+class Rate(models.Model):
+    user = models.ForeignKey("User", verbose_name="rate_user", on_delete=models.CASCADE)
+    subject = models.CharField(max_length=60)
+    professor = models.CharField(max_length=45)
+    rate = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    title = models.TextField()
+    content = models.TextField()
