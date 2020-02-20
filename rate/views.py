b4810c0e5986920c828bc2bdd19e5bc6ac15a0ea
jiss02 <olo5112olo@naver.com>
2020-02-20 18:55:38 +0900

    view CR 기능 구현

--- rate/views.py
+++ rate/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Rate
+from .forms import RateForm
 
-# Create your views here.
 def readAll(req, res):
-    return 404
+    rates = Rate.objects.all()
+    return render(req, 'rate/list.html', {'rates':rates})
 
 def create(req, res):
-    return 404
+    if req.method == "POST":
+        form = RateForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rate_list')
+    else:
+        form = RateForm()
+    return render(req, 'rate/create.html', {"form":form})
