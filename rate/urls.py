06158d0f00bf6df97afe365e939377ab7590ec4e
YOOJIYEON <hellouz818@likelion.org>
2020-02-21 21:34:58 +0900

    수정하기 url 등록

--- rate/urls.py
+++ rate/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.readAll, name="rate_list"),
     path('create/', views.create, name="rate_create"),
+    #수정 url 추가
+    path('edit/', views.edit, name="rate_edit"),
+    path('update/<int:rate_id>', views.update, name="rate_update"),
 ]
