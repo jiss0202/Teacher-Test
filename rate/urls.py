2fc05a84020b14f08dd75e5bce9957f2f34cf11f
YOOJIYEON <hellouz818@likelion.org>
2020-02-21 22:58:29 +0900

    삭제하기 url 등록

--- rate/urls.py
+++ rate/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.readAll, name="rate_list"),
     path('create/', views.create, name="rate_create"),
-    #수정 url 추가
     path('edit/', views.edit, name="rate_edit"),
     path('update/<int:rate_id>', views.update, name="rate_update"),
+    #삭제 url 추가
+    path('delete/<int:rate_id>', views.delete, name="rate_delete"),
 ]
