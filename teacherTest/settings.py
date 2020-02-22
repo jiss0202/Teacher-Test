22a05471b6d206881de430d86a0d9e98981209bf
eneun-lion <wtcat@likelion.org>
2020-02-22 16:01:05 +0900

    allowed host 수정

--- teacherTest/settings.py
+++ teacherTest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$c*0g=yya8!9z$bm7e5f5s9#wsl7qj
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
